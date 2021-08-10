cbc499ae1693a5af3eccbcf083bd87f3139b1dbc
Dinitha Kulasooriya <dinitha45033@gmail.com>
2021-08-10 14:04:33 +0530

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LogInApp.Models;
+using System.Data.SqlClient;
+
+namespace LogInApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        private SqlDataReader dr;
+
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+        void connectionString()
+        {
+            con.ConnectionString = "data source=LAPTOP-SUT349NN; database=LoginDB; integrated security = SSPI";
+        }
+        [HttpPost]
+
+        public ActionResult Verify(Account acc)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings[@"ConnectionString1"].ConnectionString;
+            
+            
+            //connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from Users where UserName='"+acc.UserName+"' and password='"+acc.Password+"'";
+            dr = com.ExecuteReader();
+
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Home");
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+
+            
+            
+        }
+    }
+}
